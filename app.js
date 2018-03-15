0cab6ac3347e90855f384b36918ec513e6f559f4
jadiago <jadiago@cpi.com.ph>
2018-03-15 11:05:22 +0800

    Some styling and status catching
    Change some styling on the modal and also catch API if returned JSON data is not a success.

--- app.js
+++ app.js
@@ -10,9 +10,26 @@
         xhr.onload = function() {
             if (this.status === 200) {
                 const jokeData = JSON.parse(this.responseText);
-                const jokeHtmlElement = `<p class="lead text-center chuck-norris-joke">"${jokeData.value.joke}"</p>`;
-                const jokeContainer = document.querySelector('#cn-joke-container');
+                let jokeHtmlElement;
+                let jokeContainer;
+
+                if (jokeData.type === 'success') {
+                    jokeHtmlElement = `<p class="lead text-center chuck-norris-joke">"${jokeData.value.joke}"</p>`;
+                    jokeContainer = document.querySelector('#cn-joke-container');
+                } else {
+                    jokeHtmlElement = `<p class="lead text-center chuck-norris-joke">"Whoops! I think something is wrong is with Chuck's API Database. Maybe he broke it I don't know. Maybe next time?"</p>`;
+                    jokeContainer = document.querySelector('#cn-joke-container');
+                    document.querySelector('.another-one').remove();
+                }
                 jokeContainer.innerHTML = jokeHtmlElement;
+
+                $('#chucks-modal').on('show.bs.modal', function (e) {
+                    $('.modal .modal-dialog').attr('class', 'modal-dialog bounceIn animated');
+                });
+
+                $('#chucks-modal').on('hide.bs.modal', function (e) {
+                    $('.modal .modal-dialog').attr('class', 'modal-dialog flipOutX animated');
+                })
 
                 setTimeout(() => {
                     $('#chucks-modal').modal('show');

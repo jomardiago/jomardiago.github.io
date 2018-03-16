0814fabd272d77ada4ca4de872417cf56bd9ec71
jadiago <jadiago@cpi.com.ph>
2018-03-16 15:09:04 +0800

    Fix secure connection for my weather api

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ class Weather {
     constructor() {};
 
     async getWeather() {
-        const response = await fetch(`http://api.wunderground.com/api/26542c3b423df29e/conditions/q/zmw:00000.68.98531.json`);
+        const response = await fetch(`https://api.wunderground.com/api/26542c3b423df29e/conditions/q/zmw:00000.68.98531.json`);
         const weatherData = await response.json();
 
         return weatherData.current_observation;
